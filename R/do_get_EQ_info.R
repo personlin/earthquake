9e2431ac8ecb3c63ff1c74449030b90e970e746b
Person Lin <personlin@gmail.com>
2020-01-31 15:54:26 +0800

    add Jamacia earthquake

--- R/do_get_EQ_info.R
+++ R/do_get_EQ_info.R
@@ -20,6 +20,9 @@ get.USGS.event.geojson("us6000522g")
 #2020-01-07 波多黎各地震
 get.USGS.event.geojson("us70006vll")
 
+#2020-01-28 牙買加地震
+get.USGS.event.geojson("us60007idc")
+
 # Get EQ info from GFZ ----
 
 # 2019-08-08 宜蘭地震
@@ -27,3 +30,6 @@ get.GFZ("gfz2019pjye")
 
 # 2020-01-07 波多黎各地震
 get.GFZ("gfz2020alpr")
+
+# 2020-01-28 牙買加地震
+get.GFZ("gfz2020byun")
